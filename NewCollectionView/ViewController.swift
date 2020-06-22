c6d3ccacdcbd65c9f9ec47bc68a86f13967e2f40
Moroz Pavel <vferme2015@gmail.com>
2020-06-22 10:05:04 +0300

    Initial Commit

---
+++ NewCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewCollectionView
+//
+//  Created by Pavel Moroz on 22.06.2020.
+//  Copyright © 2020 Pavel Moroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
